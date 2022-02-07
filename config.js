2d75c220cdc98da8d736718e31232e6f39780d31
James McNeil <mcneil_james@hotmail.com>
2022-02-07 13:31:02 +0000

    moved config file

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
-import 'firebase/storage'
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyByrGgGO8m_48NdzDz9Kdj7Oqbfio-i1dU',
@@ -12,15 +10,4 @@ const firebaseConfig = {
   appId: '1:949679346504:web:9e6e23e73e8e4d4c71997d',
 }
 
-// init firebase
-firebase.initializeApp(firebaseConfig)
-
-// init services
-const projectFirestore = firebase.firestore()
-const projectAuth = firebase.auth()
-const projectStorage = firebase.storage()
-
-// timestamp
-const timestamp = firebase.firestore.Timestamp
-
-export { projectFirestore, projectAuth, projectStorage, timestamp }
+export const db = getFirestore()
