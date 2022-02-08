6e3eaaf65d7b5d17bb0a32dabca23a0d97e2f342
James McNeil <mcneil_james@hotmail.com>
2022-02-08 13:48:21 +0000

    moved assets

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyByrGgGO8m_48NdzDz9Kdj7Oqbfio-i1dU',
   authDomain: 'house-marketplace-app-f366f.firebaseapp.com',
@@ -10,4 +12,6 @@ const firebaseConfig = {
   appId: '1:949679346504:web:9e6e23e73e8e4d4c71997d',
 }
 
+// Initialize Firebase
+initializeApp(firebaseConfig)
 export const db = getFirestore()
