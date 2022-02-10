fa7792c406a2e491f6936f0106314e91ab09905b
James McNeil <mcneil_james@hotmail.com>
2022-02-10 20:06:13 +0000

    boilerplate and basic component state setup

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,4 +1,19 @@
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { ReactComponent as ArrorRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+
 const SignIn = () => {
+  const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const { email, password } = formData
+
+  const navigate = useNavigate()
+
   return (
     <div>
       <h1>SignIn</h1>
