943dda2f3f7023e803621a99f68fef5403534423
James McNeil <mcneil_james@hotmail.com>
2022-02-13 20:12:35 +0000

    imported toastify css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // React Components
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 // Components & Pages
 import Navbar from './components/Navbar'
