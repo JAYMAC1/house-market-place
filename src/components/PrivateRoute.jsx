97addce1560aa2eff2d576851f4340ad1ed04b99
James McNeil <mcneil_james@hotmail.com>
2022-02-16 19:55:38 +0000

    tested true

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 
 const PrivateRoute = () => {
-  const loggedIn = false
+  const loggedIn = true
   return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />
 }
 
