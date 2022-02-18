f23e683ed0fcc46f9d9ac45cd0c513017bbfb1aa
James McNeil <mcneil_james@hotmail.com>
2022-02-18 14:51:51 +0000

    added listing details

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as DeleteIcon } from '../assets/svg/deleteIcon.svg'
 import bedIcon from '../assets/svg/bedIcon.svg'
 import bathTubIcon from '../assets/svg/bathtubIcon.svg'
 
-const ListingItem = ({ listing, id }) => {
+const ListingItem = ({ listing, id, onDelete }) => {
   return (
     <li className='categoryListing'>
       <Link
@@ -15,7 +15,41 @@ const ListingItem = ({ listing, id }) => {
           alt={listing.name}
           className='categoryListingImg'
         />
+        <div className='categoryListingDetails'>
+          <p className='categoryListingLocation'>{listing.location}</p>
+          <p className='categoryListingName'>{listing.name}</p>
+          <p className='categoryListingPrice'>
+            £
+            {listing.offer
+              ? listing.discountedPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+              : listing.regularPrice}
+            {listing.type === 'rent' && ' / Month'}
+          </p>
+          <div className='categoryListingInfoDiv'>
+            <img src={bedIcon} alt='bed' />
+            <p className='categoryListingInfoText'>
+              {listing.bedrooms > 1
+                ? `${listing.bedrooms} Bedrooms`
+                : '1 bedroom'}
+            </p>
+            <img src={bathTubIcon} alt='bed' />
+            <p className='categoryListingInfoText'>
+              {listing.bathrooms > 1
+                ? `${listing.bathrooms} Bathrooms`
+                : '1 Bathroom'}
+            </p>
+          </div>
+        </div>
       </Link>
+      {onDelete && (
+        <DeleteIcon
+          className='removeIcon'
+          fill='rgb(231, 76, 60)'
+          handleClick={() => onDelete(listing.id, listing.name)}
+        />
+      )}
     </li>
   )
 }
