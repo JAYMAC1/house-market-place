c10ac5d52d02852387e04f2b32c22068fb4208e1
James McNeil <mcneil_james@hotmail.com>
2022-02-21 12:40:25 +0000

    initial creation of Listing Component then imported to App.js to create Route

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ForgotPassword from './pages/ForgotPassword'
 import PrivateRoute from './components/PrivateRoute'
 import Category from './pages/Category'
 import CreateListing from './pages/CreateListing'
+import Listing from './pages/Listing'
 
 function App() {
   return (
@@ -23,6 +24,10 @@ function App() {
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
           <Route path='/category/:categoryName' element={<Category />} />
+          <Route
+            path='/category/:categoryName/:listingId'
+            element={<Listing />}
+          />
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
           </Route>
