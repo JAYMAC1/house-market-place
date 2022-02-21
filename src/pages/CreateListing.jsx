aac21b529c0131561a8697174d8144865c80ff36
James McNeil <mcneil_james@hotmail.com>
2022-02-21 12:35:24 +0000

    changed location to use address from create listing form

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -123,7 +123,6 @@ const CreateListing = () => {
     } else {
       geolocation.lat = latitude
       geolocation.lng = longitude
-      location = address
     }
 
     // store image in firebase storage
@@ -177,9 +176,11 @@ const CreateListing = () => {
       timestamp: serverTimestamp(),
     }
 
+    formDataCopy.location = address
     delete formDataCopy.images
     delete formDataCopy.address
-    location && (formDataCopy.location = location)
+    // location && (formDataCopy.location = location)
+
     !formDataCopy.offer && delete formDataCopy.discountedPrice
 
     const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
