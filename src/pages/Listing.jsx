e4217f49ca11f7f7baf4d852e80e30e10b9cdcf0
James McNeil <mcneil_james@hotmail.com>
2022-02-21 18:35:13 +0000

    Added Swiper to scroll images within the Listing comp

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -11,7 +11,10 @@ import { db } from '../firebase/config'
 import Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
-
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.css'
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 const Listing = () => {
   const [listing, setListing] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
@@ -41,7 +44,19 @@ const Listing = () => {
 
   return (
     <main>
-      {/* Slider to go here */}
+      <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+        {listing.imageUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            {' '}
+            <div
+              style={{
+                background: `url(${listing.imageUrls[index]}) center no-repeat`,
+                backgroundSize: 'cover',
+              }}
+              className='swiperSlideDiv'></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
       <div
         className='shareIconDiv'
         onClick={() => {
