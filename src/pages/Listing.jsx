1dcd844aaeeee77cbbc747db4d6d5696996b503c
James McNeil <mcneil_james@hotmail.com>
2022-02-21 13:50:01 +0000

    created Listing page component for single  Listing

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -34,7 +34,71 @@ const Listing = () => {
     fetchListing()
   }, [params.listingId])
 
-  return <div>Listing</div>
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  return (
+    <main>
+      {/* Slider to go here */}
+      <div
+        className='shareIconDiv'
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href)
+          setShareLinkCopied(true)
+          setTimeout(() => {
+            setShareLinkCopied(false)
+          }, 2000)
+        }}>
+        <img src={shareIcon} alt='' />
+      </div>
+      {shareLinkCopied && <p className='linkCopied'>Link Copied</p>}
+      <div className='listingDetails'>
+        <p className='listingName'>
+          {listing.name} - £
+          {listing.offer
+            ? listing.discountedPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+            : listing.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+        </p>
+        <p className='listingLocation'>{listing.location}</p>
+        <p className='listingType'>
+          For {listing.type === 'rent' ? 'Rent' : 'Sale'}
+        </p>
+        {listing.offer && (
+          <p className='discountPrice'>
+            £{listing.regularPrice - listing.discountedPrice} discount
+          </p>
+        )}
+        <ul className='listingDetailsList'>
+          <li>
+            {listing.bedrooms > 1
+              ? `${listing.bedrooms} Bedrooms`
+              : `1 bedroom`}
+          </li>
+          <li>
+            {listing.bathrooms > 1
+              ? `${listing.bathrooms} Bathrooms`
+              : `1 Bathroom`}
+          </li>
+          <li>{listing.parking && 'Off road parking'}</li>
+          <li>{listing.furnished && 'Furnished'}</li>
+        </ul>
+        <p className='listingLocationTitle'>Location</p>
+        {/* MAP */}
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`}
+            className='primaryButton'>
+            Contact Landlord
+          </Link>
+        )}
+      </div>
+    </main>
+  )
 }
 
 export default Listing
