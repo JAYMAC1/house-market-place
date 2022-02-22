e58ef6862ccac09a719afb7bb53540a2dca75ada
James McNeil <mcneil_james@hotmail.com>
2022-02-22 09:41:43 +0000

    addec paginatin to Offers page

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -17,6 +17,7 @@ import ListingItem from '../components/ListingItem'
 const Offers = () => {
   const [listings, setListings] = useState(null)
   const [loading, setLoading] = useState(true)
+  const [lastFetchedListing, setLastFetchedListing] = useState(null)
 
   const params = useParams()
 
@@ -37,6 +38,9 @@ const Offers = () => {
         // execute query
         const querySnap = await getDocs(q)
 
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+        setLastFetchedListing(lastVisible)
+
         const listings = []
         querySnap.forEach((doc) => {
           listings.push({
@@ -53,6 +57,42 @@ const Offers = () => {
     }
     fetchListings()
   }, [])
+
+  // Pagination / Load More
+  const onFetchMoreListings = async () => {
+    try {
+      // Get reference
+      const listingRef = collection(db, 'listings')
+
+      // create a query
+      const q = query(
+        listingRef,
+        where('offer', '==', true),
+        orderBy('timestamp', 'desc'),
+        startAfter(lastFetchedListing),
+        limit(10)
+      )
+
+      // execute query
+      const querySnap = await getDocs(q)
+
+      const lastVisible = querySnap.doc(querySnap.docs.length - 1)
+      setLastFetchedListing(lastVisible)
+
+      const listings = []
+      querySnap.forEach((doc) => {
+        listings.push({
+          id: doc.id,
+          data: doc.data(),
+        })
+      })
+
+      setListings((prevState) => [...prevState, ...listings])
+      setLoading(false)
+    } catch (error) {
+      toast.error('Could not fetch listings')
+    }
+  }
 
   return (
     <div className='category'>
@@ -75,6 +115,13 @@ const Offers = () => {
               ))}
             </ul>
           </main>
+          <br />
+          <br />
+          {lastFetchedListing && (
+            <p className='loadMore' onClick={onFetchMoreListings}>
+              Load More
+            </p>
+          )}
         </>
       ) : (
         <p>There are no current offers.</p>
